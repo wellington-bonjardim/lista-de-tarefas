a7b647c74262fb571d8e3ac585736996d432baaf
Bonjardim <wellingtonbonjardim7@gmail.com>
2022-07-27 17:25:47 -0300

    Criando as funções dos botões de concluir e excluir tarefa

--- main.js
+++ main.js
@@ -1,41 +1,77 @@
-const input = document.querySelector('.nova-tarefa-input')
+const inputTarefa = document.querySelector('.nova-tarefa-input')
 const botaoAdicionar = document.querySelector('.nova-tarefa-botao')
 const tarefaAdicionada = document.querySelector('.tarefa-adicionada')
 
-const validaInput = () => input.value.trim().length > 0
+const validaInput = () => inputTarefa.value.trim().length > 0
 
 const adicionarTarefa = () => {
     const inputValido = validaInput()
 
     if(!inputValido) {
-        return input.classList.add('erro')
+        return inputTarefa.classList.add('erro')
     }
 
     const tarefaPendente = document.createElement('div')
     tarefaPendente.classList.add('novo-item')
 
     const tarefaConteudo = document.createElement('p')
-    tarefaConteudo.innerText = input.value
+    tarefaConteudo.innerText = inputTarefa.value
+
+    const divBotoes = document.createElement('div')
+    
+    const concluirTarefa = document.createElement('i')
+    concluirTarefa.classList.add('fa-solid')
+    concluirTarefa.classList.add('fa-check')
+
+    concluirTarefa.addEventListener('click', () => concluirTarefaClick(tarefaConteudo))
 
     const deletarTarefa = document.createElement('i')
     deletarTarefa.classList.add('fa-solid')
     deletarTarefa.classList.add('fa-trash-can')
 
+    deletarTarefa.addEventListener('click', () => deletarTarefaClick(tarefaPendente, tarefaConteudo))
+
     tarefaPendente.appendChild(tarefaConteudo)
-    tarefaPendente.appendChild(deletarTarefa)
+    tarefaPendente.appendChild(divBotoes)
+    divBotoes.appendChild(concluirTarefa)
+    divBotoes.appendChild(deletarTarefa)
 
     tarefaAdicionada.appendChild(tarefaPendente)
 
-    input.value = ''
+    inputTarefa.value = ''
+
+}
+
+const concluirTarefaClick = (tarefaConteudo) => {
+    const tarefas = tarefaAdicionada.childNodes
+
+    for(const tarefa of tarefas) {
+        const tarefaAtualClicada = tarefa.firstChild.isSameNode(tarefaConteudo)
+        if(tarefaAtualClicada) {
+            tarefa.firstChild.classList.toggle('finalizada')
+        }
+    }
+
+}
+
+const deletarTarefaClick = (tarefaPendente, tarefaConteudo) => {
+    const tarefas = tarefaAdicionada.childNodes
+
+    for(const tarefa of tarefas) {
+        const tarefaAtualClicada = tarefa.firstChild.isSameNode(tarefaConteudo)
+        if(tarefaAtualClicada) {
+            tarefaPendente.remove()
+        }
+    }
 }
 
 const mudaEstadoInput = () => {
     const inputValido = validaInput()
 
     if(inputValido) {
-        return input.classList.remove('erro')
+        return inputTarefa.classList.remove('erro')
     }
 }
 
 botaoAdicionar.addEventListener('click', () => adicionarTarefa())
-input.addEventListener('change', () => mudaEstadoInput())
+inputTarefa.addEventListener('change', () => mudaEstadoInput())
